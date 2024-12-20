47b0891382fee2e7a398dc47374d4a16d06fbb03
Gayathrigeetha-Thangavel <gayathrigeetha.thangavel@gmail.com>
2024-12-20 14:46:57 +0100

    Validation added for TodoList

--- TodoList/TodoItem.cs
+++ TodoList/TodoItem.cs
@@ -1,10 +1,12 @@
-﻿namespace BlazorTodoList.TodoList
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorTodoList.TodoList
 {
     public class TodoItem
     {
+        [Required(ErrorMessage = "Title should not be empty")]
+        [StringLength(50)]
         public string? Title { get; set; }
         public bool IsDone { get; set; }
-
-
     }
 }
